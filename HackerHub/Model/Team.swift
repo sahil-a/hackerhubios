c32e8d76a8fecda656cea86e280f8711c9196fc1
Sahil Ambardekar <ambardekarsahil@gmail.com>
2019-03-23 12:40:39 -0400

    added model files

---
+++ HackerHub/Model/Team.swift
@@ -0,0 +1,9 @@
+//
+//  Team.swift
+//  HackerHub
+//
+//  Created by Sahil Ambardekar on 3/23/19.
+//  Copyright © 2019 dankcoders. All rights reserved.
+//
+
+import Foundation
---
+++ HackerHub/Model/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  HackerHub
+//
+//  Created by Sahil Ambardekar on 3/23/19.
+//  Copyright © 2019 dankcoders. All rights reserved.
+//
+
+import Foundation
